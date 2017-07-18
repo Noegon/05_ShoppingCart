8ba2006be7999482d65308ed27e1a3836f3c98a6
Alex Stafeyev <noegon@yandex.ru>
2017-07-19 01:00:48 +0300

    [INIT]Initial commit

---
+++ ShoppingCart/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ShoppingCart
+//
+//  Created by Alex on 18.07.17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ShoppingCart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingCart
+//
+//  Created by Alex on 18.07.17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
