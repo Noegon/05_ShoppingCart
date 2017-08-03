33725c36d62932c2dc83349dec358db378c880ec
Alexey Stafeyev <noegon@yandex.ru>
2017-08-03 09:09:38 +0300

    [MENU_IS_READY]

---
+++ ShoppingCart/View/TableCells/NGNMenuTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NGNMenuTableViewCell.h
+//  ShoppingCart
+//
+//  Created by Alexey Stafeyev on 03.08.17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGNMenuTableViewCell : UITableViewCell
+
+@end
